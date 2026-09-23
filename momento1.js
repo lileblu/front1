9efc7dcfaee20968393c4a5e7b65775ddc83317e
lileblu <cironaranjoveronica@gmail.com>
2026-09-23 18:25:07 -0500

    feat: se mete toda la logica dentro de la función validasAcceso

--- momento1.js
+++ momento1.js
@@ -1,3 +1,6 @@
+
+function validarAcceso(){
+
 const USUARIO_CORRECTO = "admin";
 const CONTRASENA_CORRECTA = "0225";
 let cantidadIntentos = 3;
@@ -7,16 +10,21 @@ while (cantidadIntentos>0) {
     let usuario = prompt(`Ingrese su usuario`);
     let contrasena = prompt(`Ingrese su contraseña`);
    
-    if ( usuario == USUARIO_CORRECTO & contrasena == CONTRASENA_CORRECTA) {
+    if ( usuario === USUARIO_CORRECTO & contrasena === CONTRASENA_CORRECTA) {
         console.log(`Bienvenido al sistema!`);
         break;
     } else { 
         cantidadIntentos--;
         console.log(`Datos incorrectos, te quedan ${cantidadIntentos} intentos` );
-        if (cantidadIntentos == 0) {
+        if (cantidadIntentos === 0) {
             console.log(`Usuario bloqueado`)
         }
     }  
 } 
+}
+
+validarAcceso();
+
+
 
  
